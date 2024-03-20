4df0a8285443b7422cb691ae9c0c056d139a9f3b
mynamescheff <92156558+mynamescheff@users.noreply.github.com>
2024-03-20 14:09:50 +0100

    updated

--- Project5/bkk.h
+++ Project5/bkk.h
@@ -1,18 +1,14 @@
 #pragma once
-#ifndef BEZKONSTRUKTORAKOPIUJACEGO_H
-#define BEZKONSTRUKTORAKOPIUJACEGO_H
 
 class BezKonstruktoraKopiujacego {
 public:
     int* wsk;
 
-    BezKonstruktoraKopiujacego(int wartosc) {
-        wsk = new int(wartosc);
-    }
+    BezKonstruktoraKopiujacego(int wartosc) : wsk(new int(wartosc)) {}
+
+    BezKonstruktoraKopiujacego(const BezKonstruktoraKopiujacego& kopia) : wsk(new int(*(kopia.wsk))) {}
 
     ~BezKonstruktoraKopiujacego() {
         delete wsk;
     }
 };
-
-#endif
--- Project5/zkk.h
+++ Project5/zkk.h
@@ -1,22 +1,14 @@
 #pragma once
-#ifndef ZKONSTRUKTOREMKOPIUJACYM_H
-#define ZKONSTRUKTOREMKOPIUJACYM_H
 
 class ZKonstruktoremKopiujacym {
 public:
     int* wsk;
 
-    ZKonstruktoremKopiujacym(int wartosc) {
-        wsk = new int(wartosc);
-    }
+    ZKonstruktoremKopiujacym(int wartosc) : wsk(new int(wartosc)) {}
 
-    ZKonstruktoremKopiujacym(const ZKonstruktoremKopiujacym& kopia) {
-        wsk = new int(*(kopia.wsk));
-    }
+    ZKonstruktoremKopiujacym(const ZKonstruktoremKopiujacym& kopia) : wsk(new int(*(kopia.wsk))) {}
 
     ~ZKonstruktoremKopiujacym() {
         delete wsk;
     }
 };
-
-#endif
